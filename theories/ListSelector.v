07072a93f368550b1e616160c01ee1812b52fb5c
k32 <10274441+k32@users.noreply.github.com>
2025-04-01 01:51:41 +0200

    Simplify pick

--- theories/ListSelector.v
+++ theories/ListSelector.v
@@ -5,20 +5,20 @@ Import ListNotations.
 Section defn.
   Context {A : Type}.
 
-  Inductive Pick : list A -> (A * list A) -> Prop :=
+  Inductive Pick : list A -> A -> list A -> Prop :=
   | pick_this : forall a l,
-      Pick (a :: l) (a, l)
+      Pick (a :: l) a l
   | pick_next : forall a b l l',
-      Pick l (a, l') ->
-      Pick (b :: l) (a, b :: l').
+      Pick l a l' ->
+      Pick (b :: l) a (b :: l').
 End defn.
 
 Section tests.
-  Goal forall a,
-      Pick [1; 2] a ->
-      a = (1, [2]) \/ a = (2, [1]).
+  Goal forall a l',
+      Pick [1; 2] a l' ->
+      (a = 1 /\ l' = [2]) \/ (a = 2 /\ l' = [1]).
   Proof.
-    intros a H.
+    intros a l' H.
     inversion_clear H.
     - now left.
     - inversion_clear H0.
