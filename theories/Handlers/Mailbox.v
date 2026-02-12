57d3e3dc1a451266e23b281f7971978ab3bb8466
k32 <10274441+k32@users.noreply.github.com>
2026-02-12 19:43:03 +0100

    Make mailbox "dynamically typed" based on pid.

--- theories/Handlers/Mailbox.v
+++ theories/Handlers/Mailbox.v
@@ -13,32 +13,52 @@ From Hammer Require Import
   Tactics.
 
 Section defn.
-  Context (AppMessage : Type).
-
-  Inductive Message :=
+  Inductive Message {AppMessage} :=
   | appmsg : AppMessage -> Message.
 
-  Definition Mailbox := @Queue Message.
+  (** Contents of the mailbox *)
+  Record Mailbox := {
+      mb_t : Type;
+      mb_q : @Queue (@Message mb_t);
+    }.
 
+  (** Handler state *)
   Let t := Pid.FMap.t Mailbox.
 
+  (** "Address" of the mailbox *)
+  Record Address {mba_t : Set} := {
+      mba_pid : PID;
+      (* Address record carries the proof that the mailbox of
+      [mba_pid] process is what we expect. This information is used at
+      the compile time for type checking. *)
+      mba_t_proof (mboxes : t) :
+      match Pid.FMap.find mba_pid mboxes with
+      | None => True
+      | Some {| mb_t := t |} => t = mba_t
+      end
+    }.
+
   Inductive MBReq : Type :=
-  | send : Message -> PID -> MBReq.
+  | send {T : Set} : @Message T -> @Address T -> MBReq.
 
   Definition MBRet (req : MBReq) : Type :=
     match req with
     | send _ _ => True
     end.
 
-  Definition do_send_msg (msg : Message) (to : PID) (mboxes : t) : t :=
-    match Pid.FMap.find to mboxes with
-    | Some mbox =>
-        Pid.FMap.add to (push msg mbox) mboxes
-    | None =>
-        mboxes
-    end.
+  Definition do_send_msg {T : Set} (msg : @Message T) (to : @Address T) (mboxes : t) : t.
+    destruct to as [pid HpidT].
+    remember (Pid.FMap.find pid mboxes) as mb.
+    destruct mb as [mbox|].
+    - specialize (HpidT mboxes). rewrite <-Heqmb in HpidT.
+      destruct mbox as [mb_t mb_q].
+      subst.
+      set (mbox' := {| mb_t := T; mb_q := push msg mb_q |}).
+      exact (Pid.FMap.add pid mbox' mboxes).
+    - exact mboxes.
+  Defined.
 
-  Program Definition send_ (msg : Message) (to : PID) : MFunRet True t :=
+  Program Definition send_ {T : Set} (msg : @Message T) (to : @Address T) : MFunRet True t :=
     {| morphism s x :=
          x = (I, do_send_msg msg to s)
     |}.
@@ -51,9 +71,7 @@ Section defn.
     | send msg to => send_ msg to
     end.
 
-  Check Pid.FMap.Equiv.
-
-  Let mailboxes_equiv := @Pid.FMap.Equiv Mailbox queue_equiv.
+  (* Let mailboxes_equiv := @Pid.FMap.Equiv Mailbox queue_equiv. *)
 
   Instance mailboxHandler : @IOHandler MBReq MBRet :=
     {|
